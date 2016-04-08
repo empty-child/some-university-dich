b986214dc3a4c70f53184c536864d51806291278
empty-child <afanasov.p@yandex.ru>
2016-04-08 20:41:46 +0300

    реализация алгоритма Марра-Хилдрет

--- sever_lab/geometric_tranformation_2.py
+++ sever_lab/geometric_tranformation_2.py
@@ -3,54 +3,43 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-log_mask = np.array([[2, 2, 4, 2, 2],
-                     [2, -1, -5, -1, 2],
-                     [4, -5, -16, -5, 4],
-                     [2, -1, -5, -1, 2],
-                     [2, 2, 4, 2, 2]], "i")
+log_mask = np.array([[0,0,3,2,2,2,3,0,0],
+                      [0,2,3,5,5,5,3,2,0],
+                      [3,3,5,3,0,3,5,3,3],
+                      [2,5,3,-12,-23,-12,3,5,2],
+                      [2,5,0,-23,-40,-23,0,5,2],
+                      [2,5,3,-12,-23,-12,3,5,2],
+                      [3,3,5,3,0,3,5,3,3],
+                      [0,2,3,5,5,5,3,2,0],
+                      [0,0,3,2,2,2,3,0,0]], "i")
 
 
-def convolution(matr, img):
+def convolution_from_grayscale(matr, img):
+    koef = len(matr)//2
     out_img = np.copy(img)
     rows, columns = img.shape[:2]
-    for i in range(columns)[2:columns-2]:
-        for j in range(rows)[2:rows-2]:
+    ar = np.zeros([rows, columns], dtype="i2")
+    for i in range(columns)[koef:columns-koef]:
+        for j in range(rows)[koef:rows-koef]:
             x = 0
-            matrsum = [0, 0, 0]
-            for i2 in [j-2, j-1, j, j+1, j+2]:
+            matrsum = 0
+            for j2 in range(-koef, koef+1):
                 y = 0
-                for j2 in [i-2, i-1, i, i+1, i+2]:
-                    matrsum[0] += img.item(j2, i2, 0)*matr[x][y]
-                    matrsum[1] += img.item(j2, i2, 1) * matr[x][y]
-                    matrsum[2] += img.item(j2, i2, 2) * matr[x][y]
+                for i2 in range(-koef, koef+1):
+                    matrsum += img.item(j+j2, i+i2) * matr[x][y]
                     y += 1
                 x += 1
-            out_img.itemset((j, i, 0), math.fabs(matrsum[0]))
-            out_img.itemset((j, i, 1), math.fabs(matrsum[1]))
-            out_img.itemset((j, i, 2), math.fabs(matrsum[2]))
-
-            # out_img.itemset((j, i, 0), math.fabs((img.item(j-1, i-1, 0)*matr[0][0]+img.item(j-1, i, 0)*matr[0][1]+
-            #                          img.item(j-1, i+1, 0)*matr[0][2]+img.item(j, i-1, 0)*matr[1][0]+
-            #                          img.item(j, i, 0)*matr[1][1]+img.item(j, i+1, 0)*matr[1][2]+
-            #                          img.item(j+1, i-1, 0)*matr[2][0]+img.item(j+1, i, 0)*matr[2][1]+
-            #                          img.item(j+1, i+1, 0)*matr[2][2])))
-            # out_img.itemset((j, i, 1), math.fabs((img.item(j-1, i-1, 1)*matr[0][0]+img.item(j-1, i, 1)*matr[0][1]+
-            #                          img.item(j-1, i+1, 1)*matr[0][2]+img.item(j, i-1, 1)*matr[1][0]+
-            #                          img.item(j, i, 1)*matr[1][1]+img.item(j, i+1, 1)*matr[1][2]+
-            #                          img.item(j+1, i-1, 1)*matr[2][0]+img.item(j+1, i, 1)*matr[2][1]+
-            #                          img.item(j+1, i+1, 1)*matr[2][2])))
-            # out_img.itemset((j, i, 2), math.fabs((img.item(j-1, i-1, 2)*matr[0][0]+img.item(j-1, i, 2)*matr[0][1]+
-            #                          img.item(j-1, i+1, 2)*matr[0][2]+img.item(j, i-1, 2)*matr[1][0]+
-            #                          img.item(j, i, 2)*matr[1][1]+img.item(j, i+1, 2)*matr[1][2]+
-            #                          img.item(j+1, i-1, 2)*matr[2][0]+img.item(j+1, i, 2)*matr[2][1]+
-            #                          img.item(j+1, i+1, 2)*matr[2][2])))
+            ar[j-koef][i-koef] = matrsum
+            out_img.itemset((j, i), 0) if ar[j][i] > 0 else out_img.itemset((j, i), 255)
     return out_img
 
-img = cv.imread("3.jpg")
-img = cv.resize(img,(555,555))
-img2 = convolution(log_mask, img)
+img = cv.imread("1.jpg")
+img2 = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+img2 = convolution_from_grayscale(log_mask, img2)
+# cv.imshow("img", img2)
+# cv.waitKey()
 plt.subplot(1,2,1),plt.imshow(img)
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(1,2,2),plt.imshow(img2)
-plt.title('LoG'), plt.xticks([]), plt.yticks([])
+plt.title('Marr-Hildreth Algorithm'), plt.xticks([]), plt.yticks([])
 plt.show()
